e48d11d76cb3d7cf21face7068e26a7fffdb6ae7
Rohan <rjha09307@gmail.com>
2026-01-01 18:35:09 +0530

    added getGithubToken function

---
+++ module/github/lib/github.ts
@@ -0,0 +1,24 @@
+import { Octokit } from "octokit";
+import { auth } from "@/lib/auth";
+import prisma from "@/lib/db";
+import { headers } from "next/headers";
+
+
+export const getGithubToken = async () => {
+    const session = await auth.api.getSession({
+      headers: await headers(),
+    })
+     if(!session){
+           throw new Error("Unauthorized")
+     }
+     const account = await prisma.account.findFirst({
+      where:{
+        userId: session.user.id,
+        providerId: "github",
+      }
+     })
+     if(!account?.accessToken){
+      throw new Error("No access token found")
+     }
+     return account.accessToken
+    }
